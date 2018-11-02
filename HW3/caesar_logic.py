ecf52048992f8364c2cb412a60c29b7e7ec2de02
chugunovanadi <tchugunowa.nadi@gmail.com>
2018-11-02 23:52:19 +0300

    homework3

--- HW3/caesar_logic.py
+++ HW3/caesar_logic.py
@@ -1,10 +1,13 @@
 def encrypt(offset, text):
     res = []
+    if offset > 25 or offset < -25:
+        raise Exception("Неверный сдвиг")
     for i in text:
-        if not (65 <= ord(i) <= 90) and not (97 <= ord(i) <= 122):
+        if (not (ord('A') <= ord(i) <= ord('Z')) and
+                not (ord('a') <= ord(i) <= ord('z'))):
             res.append(i)
         else:
-            if 97 <= ord(i) <= 122:
+            if ord('a') <= ord(i) <= ord('z'):
                 n = ord(i) + offset
                 if n > ord('z'):
                     n = n - ord('z') + ord('a') - 1
@@ -17,8 +20,7 @@ def encrypt(offset, text):
                 else:
                     char = chr(n)
                     res.append(char)
-
-            if 65 <= ord(i) <= 90:
+            if ord('A') <= ord(i) <= ord('Z'):
                 n = ord(i) + offset
                 if n > ord('Z'):
                     n = n - ord('Z') + ord('A') - 1
@@ -35,36 +37,4 @@ def encrypt(offset, text):
 
 
 def decrypt(offset, text):
-    res = []
-    for i in text:
-        if not (65 <= ord(i) <= 90) and not (97 <= ord(i) <= 122):
-            res.append(i)
-        else:
-            if 97 <= ord(i) <= 122:
-                n = ord(i) - offset
-                if n > ord('z'):
-                    n = n - ord('z') + ord('a') - 1
-                    char = chr(n)
-                    res.append(char)
-                elif n < ord('a'):
-                    n = ord('z') - ord('a') + n + 1
-                    char = chr(n)
-                    res.append(char)
-                else:
-                    char = chr(n)
-                    res.append(char)
-
-            if 65 <= ord(i) <= 90:
-                n = ord(i) - offset
-                if n > ord('Z'):
-                    n = n - ord('Z') + ord('A') - 1
-                    char = chr(n)
-                    res.append(char)
-                elif n < ord('A'):
-                    n = ord('Z') - ord('A') + n + 1
-                    char = chr(n)
-                    res.append(char)
-                else:
-                    char = chr(n)
-                    res.append(char)
-    return ''.join(res)
+    return encrypt(-offset, text)
--- HW3/caesar_test.py
+++ HW3/caesar_test.py
@@ -7,10 +7,12 @@ class Tester(TestCase):
         self.assertEqual(encrypt(3, 'Hello world'), 'Khoor zruog')
 
     def test_true_encrypt_2(self):
-        self.assertEqual(encrypt(-5, 'Moyu konus v vannoy'), 'Hjtp fjipn q qviijt')
+        self.assertEqual(encrypt(-5, 'Moyu konus v vannoy'),
+                         'Hjtp fjipn q qviijt')
 
     def test_true_encrypt_3(self):
-        self.assertEqual(encrypt(25, 'Vylivayu vino v rakovinu'), 'Uxkhuzxt uhmn u qzjnuhmt')
+        self.assertEqual(encrypt(25, 'Vylivayu vino v rakovinu'),
+                         'Uxkhuzxt uhmn u qzjnuhmt')
 
     def test_true_encrypt_4(self):
         self.assertEqual(encrypt(0, 'With Halloween'), 'With Halloween')
@@ -19,31 +21,41 @@ class Tester(TestCase):
         self.assertEqual(encrypt(6, '(,,,)=(^.^)=(,,,)'), '(,,,)=(^.^)=(,,,)')
 
     def test_true_encrypt_6(self):
-        self.assertEqual(encrypt(7, 'PoStAvtE zachet AVTOMATOM'), 'WvZaHcaL ghjola HCAVTHAVT')
+        self.assertEqual(encrypt(7, 'PoStAvtE zachet AVTOMATOM'),
+                         'WvZaHcaL ghjola HCAVTHAVT')
 
     def test_true_encrypt_7(self):
         self.assertEqual(encrypt(5, 'WYXZZZZZwyxzzzzz'), 'BDCEEEEEbdceeeee')
 
     def test_true_decrypt_1(self):
-        self.assertEqual(decrypt(3, 'Khoor zruog'), 'Hello world')
+        self.assertEqual(decrypt(3, 'Khoor zruog'),
+                         'Hello world')
 
     def test_true_decrypt_2(self):
-        self.assertEqual(decrypt(-5, 'Hjtp fjipn q qviijt'), 'Moyu konus v vannoy')
+        self.assertEqual(decrypt(-5, 'Hjtp fjipn q qviijt'),
+                         'Moyu konus v vannoy')
 
     def test_true_decrypt_3(self):
-        self.assertEqual(decrypt(25, 'Uxkhuzxt uhmn u qzjnuhmt'), 'Vylivayu vino v rakovinu')
+        self.assertEqual(decrypt(25, 'Uxkhuzxt uhmn u qzjnuhmt'),
+                         'Vylivayu vino v rakovinu')
 
     def test_true_decrypt_4(self):
         self.assertEqual(decrypt(0, 'With Halloween'), 'With Halloween')
 
     def test_true_decrypt_5(self):
-        self.assertEqual(decrypt(6, '(,,,)=(^.^)=(,,,)'), '(,,,)=(^.^)=(,,,)')
+        self.assertEqual(decrypt(6, '(,,,)=(^.^)=(,,,)'),
+                         '(,,,)=(^.^)=(,,,)')
 
     def test_true_decrypt_6(self):
-        self.assertEqual(decrypt(7, 'WvZaHcaL ghjola HCAVTHAVT'), 'PoStAvtE zachet AVTOMATOM')
+        self.assertEqual(decrypt(7, 'WvZaHcaL ghjola HCAVTHAVT'),
+                         'PoStAvtE zachet AVTOMATOM')
 
     def test_true_decrypt_7(self):
-        self.assertEqual(decrypt(5, 'BDCEEEEEbdceeeeeee'), 'WYXZZZZZwyxzzzzzzz')
+        self.assertEqual(decrypt(5, 'BDCEEEEEbdceeeeeee'),
+                         'WYXZZZZZwyxzzzzzzz')
+
+    def test_exception_encrypt(self):
+        self.assertRaises(Exception, encrypt, 26, "Hello")
 
 
 main()
